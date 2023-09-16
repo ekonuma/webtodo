1d664102d72fc8e04eb5cc51649a2f65cec50be3
ekonuma <konumaeike5618@gmail.com>
2023-09-15 22:49:44 -0300

    refactor(add SignUp method)

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -4,9 +4,8 @@ import (
 	"go-rest-api/model"
 	"go-rest-api/usecase"
 	"net/http"
-	"os"
-	"time"
 
+	"github.com/ekonuma/webtodo/model"
 	"github.com/labstack/echo/v4"
 )
 
@@ -17,10 +16,22 @@ type IUserController interface {
 	CsrfToken(c echo.Context) error
 }
 
-type userController struct{
+type userController struct {
 	userUserCase usecase.IUserUsecase
 }
 
 func NewUserController(userUserCase usecase.IUserUsecase) IUserController {
 	return &userController{userUserCase}
+}
+
+func (userController *userController) SignUp(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	userRes, err := userController.userUserCase.SignUp(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusCreated, userRes)
 }
